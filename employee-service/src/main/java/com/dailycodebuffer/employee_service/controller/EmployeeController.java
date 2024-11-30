8c4edac3f787c4bfdc81b19604f468da394da7b5
Aman Patel <aman17626@gmail.com>
2024-11-30 12:06:34 +0530

    add employee service configuration

---
+++ employee-service/src/main/java/com/dailycodebuffer/employee_service/controller/EmployeeController.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.employee_service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+}
---
+++ employee-service/src/main/java/com/dailycodebuffer/employee_service/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.employee_service.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeRepository {
+}
